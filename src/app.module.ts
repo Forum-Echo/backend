c45d9ecbd48279494292b7e38dee123161036fbb
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-08-28 20:02:13 +0200

    Added CommentsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { PostModule } from './modules/posts/post.module';
 import { AdminModule } from './modules/admin/admin.module';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { MailModule } from './modules/mail/mail.module';
+import { CommentsModule } from './modules/comments/comments.module';
 import * as dotenv from 'dotenv';
 
 dotenv.config({
@@ -25,6 +26,7 @@ dotenv.config({
       ttl: 60,
       limit: 30,
     }),
+    CommentsModule,
   ],
   controllers: [AppController],
   providers: [],
