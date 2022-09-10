8040d0aa2547e699f3dcce0d94372026c6e7f63f
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-09-10 16:22:34 +0200

    Removed ssl for dev

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
 
-const httpsOptions = {
+/*const httpsOptions = {
   key: fs.readFileSync('src/modules/environment/ssl/key.pem'),
   intermediate: fs.readFileSync('src/modules/environment/ssl/intermediate.pem'),
   cert: fs.readFileSync('src/modules/environment/ssl/cert.pem'),
-};
+};*/
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true, httpsOptions });
+  const app = await NestFactory.create(AppModule, { cors: true, /*httpsOptions*/ });
   process.env['NODE_TLS_REJECT_UNAUTHORIZED=0'] = String(0);
   app.enableCors();
   await app.listen(3000);
